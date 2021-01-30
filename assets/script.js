3d4217b34597d6fbd374b7ebf08c7c9a8d008084
DmytroQIvanov <20IvanovDmitriy03@gmail.com>
2021-01-30 10:49:52 +0200

    ...

--- assets/script.js
+++ assets/script.js
@@ -1,25 +1,50 @@
-let url = "https://api.coindesk.com/v1/bpi/currentprice/CNY.json'";
+// let url = "https://api.coindesk.com/v1/bpi/currentprice/CNY.json'";
+let url = "https://blockchain.info/ticker";
 let dataArray = "d";
-async function GetJSON() {
-  try {
-    let responce = await fetch(url);
 
-    let data = await responce.json();
-    console.log(data);
-    document.getElementById("time").innerHTML = "Time: " + data.time.updated;
-    document.getElementById("usd").innerHTML = "USD: " + data.bpi.USD.rate;
-    document.getElementById("gbp").innerHTML = "CNY: " + data.bpi.CNY.rate;
-    // document.getElementById("eur").innerHTML += data.bpi.EUR.rate;
-  } catch (e) {
-    console.log(e);
+function d(L) {
+  dataArray = L;
+  //   console.log(dataArray);
+  return dataArray;
+}
+check();
+
+async function check() {
+  setInterval(() => {
+    GetJSON().then((value) => {});
+    document.getElementById("crypto-names").classList.toggle("animation");
+    setTimeout(() => {
+      document.getElementById("crypto-names").classList.toggle("animation");
+    }, 900);
+  }, 1000);
+
+  let previorValue = "0";
+
+  function currentDate() {
+    let currentdata = new Date();
+    return currentdata.getHours() + ":" + currentdata.getMinutes();
+  }
+
+  async function GetJSON() {
+    try {
+      let responce = await fetch(url);
+
+      let data = await responce.json();
+
+      if (data.USD.last != previorValue) {
+        previorValue = data.USD.last;
+      }
+      console.log(previorValue);
+      document.getElementById("time").innerHTML = "Time: " + currentDate();
+      document.getElementById("usd").innerHTML =
+        "USD: " + data.USD.last + ` ${data.USD.symbol}`;
+      document.getElementById(
+        "gbp"
+      ).innerHTML = `GBP: ${data.GBP.last} ${data.GBP.symbol}`;
+
+      // document.getElementById("eur").innerHTML += data.bpi.EUR.rate;
+    } catch (e) {
+      console.log(e);
+    }
   }
 }
-
-GetJSON();
-setInterval(() => {
-  GetJSON();
-  document.getElementById("crypto-names").classList.toggle("animation");
-  setTimeout(() => {
-    document.getElementById("crypto-names").classList.toggle("animation");
-  }, 500);
-}, 1000);
